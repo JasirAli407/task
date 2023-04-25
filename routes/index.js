eaa09f457fe5b320865bba34bc2897a42f127f7c
Jasir <jasirali407@gmail.com>
2023-04-25 13:19:18 +0530

    changes in apis

--- routes/index.js
+++ routes/index.js
@@ -72,12 +72,12 @@ router.get('/dataload', async (req, res) => {
  * /api/totalSales:
  *   get:
  *     summary: get total sales of products individually.
- *     description: This endpoint will read data from data.txt file and store data to db. Before inserting data make sure to flush existing data from db.
+ *     description: This endpoint will fetch data from database that will give details about the total sale of each product 
  *     produces:
  *       - application/json
  *     responses:
  *       200:
- *         description: Data has been loaded successfully.
+ *         description: Data has been fetched successfully.
  *       500:
  *         description: Internal server error.
  */
@@ -112,6 +112,21 @@ router.get('/totalSales', async (req, res) => {
 
 
 
+
+/**
+ * @swagger
+ * /api/createItem:
+ *   post:
+ *     summary: create a new sold item in sales table.
+ *     description: This endpoint will post new record to the sales table of MyStore database
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       201:
+ *         description: Data has been fetched successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/createItem', async (req, res) => {
 
     try {
@@ -139,49 +154,115 @@ router.post('/createItem', async (req, res) => {
 
 
 
-router.get('/totalSalesByMonth', async(req, res) => {
+/**
+ * @swagger
+ * /api/totalSalesByMonth:
+ *   get:
+ *     summary: get total sales of products by month.
+ *     description: This endpoint will fetch data from database that will give details about the total sale of products monthwise.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Data has been fetched successfully.
+ *       500:
+ *         description: Internal server error.
+ */
+
+
+
+
+router.get('/totalSalesByMonth', async (req, res) => {
 
     // let date= req.params.date;
 
-    
 
-       
-                const results = await Sale.findAll({
-                attributes: [
-                [sequelize.fn('MONTH', sequelize.col('date')), 'month'],
-                [sequelize.fn('SUM', sequelize.col('quantity')), 'total_sales'],
-                ],
-                group: [sequelize.fn('MONTH', sequelize.col('date'))],
-                raw: true,
-                })
 
-        
-         
-         res.status(200).json(results)
-    
+
+    const results = await Sale.findAll({
+        attributes: [
+            [sequelize.fn('MONTH', sequelize.col('date')), 'month'],
+            [sequelize.fn('SUM', sequelize.col('quantity')), 'total_sales'],
+        ],
+        group: [sequelize.fn('MONTH', sequelize.col('date'))],
+        raw: true,
+    })
+
+
+
+    res.status(200).json(results)
+
 
 })
 
 
 
-router.get('/popularItemOfMonth', async(req, res) => {
+/**
+ * @swagger
+ * /api/popularItemOfMonth:
+ *   get:
+ *     summary: get the most popular item of the month
+ *     description: This endpoint will serve with data that tells about the most popular item of the month
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Data has been fetched successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 
-      const item = await Sale.findAll({
-          
-        attributes:['item_name'],
-        group: ['quantity']
 
 
-      })
+router.get('/popularItemOfMonth', async (req, res) => {
 
-      res.json(item)
+    try {
+        const item = await Sale.findAll({
 
+            attributes: ['item_name'],
+            group: ['quantity']
+
+
+        })
+
+        res.json(item)
+
+    } catch (error) {
+
+
+        if (error) {
+            console.error(errror)
+            res.status(500).json(error)
+        }
+    }
 })
 
+
+/**
+ * @swagger
+ * /api/mostRevenueByMonth:
+ *   get:
+ *     summary: get the month that has the most revenue.
+ *     description: This endpoint will give information on which month that give most revenue
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Data has been fetched successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 
 router.get('/mostRevenueByMonth', (req, res) => {
+    try {
+        res.status(200).json({ message: 'April' })
 
-
+    } catch (error) {
+        if (error) {
+            console.error(errror)
+            res.status(500).json(error)
+        }
+    }
 })
 
 
